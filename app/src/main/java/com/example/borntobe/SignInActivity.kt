66e1f50d7d52575d57387d3444fae16450b1a58c
Eun Jin Park <ej.park385@gmail.com>
2024-03-30 19:04:08 +0900

    1. 로그인 화면 레이아웃 구현 2. ViewBinding 설정

--- app/src/main/java/com/example/borntobe/SignInActivity.kt
+++ app/src/main/java/com/example/borntobe/SignInActivity.kt
@@ -14,7 +14,8 @@ class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_sign_in)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
