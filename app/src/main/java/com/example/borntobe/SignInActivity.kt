6567588d9935eef2f3277628242d911caf7aec2f
Eun Jin Park <ej.park385@gmail.com>
2024-04-16 02:23:21 +0900

    feature: Validating user input
    사용자가 입력하는 id와 pw가 지정한 패턴과 일치하는지 실시간으로 검사하는 기능 추가
    
    fix: Modified sign_up layout
    
    회원가입 화면 레이아웃의 constraint layout 제약 수정

--- app/src/main/java/com/example/borntobe/SignInActivity.kt
+++ app/src/main/java/com/example/borntobe/SignInActivity.kt
@@ -3,6 +3,8 @@ package com.example.borntobe
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
 import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
@@ -12,6 +14,8 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.borntobe.databinding.ActivitySignInBinding
 
+/** 로그인 화면
+ * 개발 담당 : 박은진 */
 class SignInActivity : AppCompatActivity() {
     // ViewBinding 사용
     private lateinit var binding: ActivitySignInBinding
@@ -31,29 +35,62 @@ class SignInActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        // utils 객체 생성
         utils = Utils(this)
 
-        // 로그인 버튼 : 사용자 입력 검사 & 사용자 정보 반환 & 메인 화면 전환
+        // 로그인 버튼 : 사용자 입력 유효성 검사 & 사용자 정보 반환 & 메인 화면 전환
         btnSignIn = binding.activitySignInBtnSignIn
         userID = binding.activitySignInEtID
         userPW = binding.activitySignInEtPW
         btnSignIn.setOnClickListener {
             // 입력 검사
             val id = userID.text.toString()
-            val password = userPW.text.toString()
+            val pw = userPW.text.toString()
             // isBlank() : 문자열 길이가 0 이거나, 공백(white space)으로만 이루어진 경우 true 리턴
             val isIDBlank = id.isBlank()
-            val isPasswordBlank = password.isBlank()
+            val isPWBlank = pw.isBlank()
             if (isIDBlank)
-                utils.showToast("ID를 입력하세요.")
-            else if (isPasswordBlank)
-                utils.showToast("비밀번호를 입력하세요.")
+                utils.showToast("ID를 입력해주세요.")
+            else if (isPWBlank)
+                utils.showToast("비밀번호를 입력해주세요.")
+            else if (!utils.isRegularID(id, userID))
+                utils.showToast("ID를 확인해주세요. ID는 6자 이상의 영문 혹은 영문과 숫자의 조합이어야 합니다.")
+            else if (!utils.isRegularPW(pw, userPW))
+                utils.showToast("비밀번호를 확인해주세요.")
             // 로그인 : 메인 화면 전환
             else {
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
             }
         }
+
+        // ID EditText : TextWatcher 연결하여 실시간 유효성 검사
+        userID.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                // 텍스트 변경될 때마다 호출 : 실시간으로 입력 값 검사 가능
+                utils.isRegularID(userID.text.toString().trim(), userID)
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
+
+        // PW EditText : TextWatcher 연결하여 실시간 유효성 검사
+        userPW.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                // 텍스트 변경될 때마다 호출 : 실시간으로 입력 값 검사 가능
+                utils.isRegularPW(userPW.text.toString().trim(), userPW)
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
 
         // 회원 가입 버튼 : 회원 가입 화면 전환
         btnSignUp = binding.activitySignInBtnSignUp
